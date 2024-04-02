a752fe06ac9c7a4d71de217f592bc89a43958556
KimDevelopment01 <165811814+KimDevelopment01@users.noreply.github.com>
2024-04-02 23:12:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    $('.left-curtain').css('width', '0%');
+    $('.right-curtain').css('width', '0%');
+
+    $('.valentines-day').click(function () {
+        $('.envelope').css({ 'animation': 'fall 3s linear 1', '-webkit-animation': 'fall 3s linear 1' });
+        $('.envelope').fadeOut(800, function () {
+            $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+
+            $('#card').css({ 'visibility': 'visible', 'opacity': 0, 'transform': 'scale(0.1)' });
+            $('#card').animate({ 'opacity': 1 }, {
+                duration: 1000, step: function (now, fx) {
+                    var scale = 1 + Math.sin(now * Math.PI) * 0.1;
+                    $(this).css('transform', 'scale(' + scale + ')');
+                }
+            });
+        });
+        $('.tap').hide();
+        $('.audio').hide();
+    });
+
+    // FLOATING HEART
+    var love = setInterval(function () {
+        var r_num = Math.floor(Math.random() * 40) + 1;
+        var r_size = Math.floor(Math.random() * 25) + 10;
+        var r_left = Math.floor(Math.random() * 100) + 1;
+        var r_bg = Math.floor(Math.random() * 25) + 100;
+        var r_time = Math.floor(Math.random() * 5) + 5;
+
+        $('.bg_heart').append("<div class='floatingHeart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+        $('.bg_heart').append("<div class='floatingHeart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+        $('.floating').each(function () {
+            var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+            var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+            if (top <= -100 || width >= 150) {
+                $(this).detach();
+            }
+        });
+    }, 500);
+
+
+
+});
